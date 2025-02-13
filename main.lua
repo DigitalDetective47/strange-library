138d7fca8f5c9231f00d2dc06a68e31fc2daa046
DigitalDetective47 <ninji2701@gmail.com>
2025-02-13 13:15:10 -0500

    add `StrangeLib.make_boosters`

--- main.lua
+++ main.lua
@@ -51,3 +51,31 @@ function StrangeLib.bulk_add(dest, src)
 end
 
 SMODS.load_file("fcalc.lua")()
+
+function StrangeLib.make_boosters(base_key, normal_poses, jumbo_poses, mega_poses, common_values, pack_size)
+    pack_size = pack_size or 3
+    for index, pos in ipairs(normal_poses) do
+        local t = copy_table(common_values)
+        t.key = base_key .. "_normal_" .. index
+        t.pos = pos
+        t.config = { extra = pack_size, choose = 1 }
+        t.cost = 4
+        SMODS.Booster(t)
+    end
+    for index, pos in ipairs(jumbo_poses) do
+        local t = copy_table(common_values)
+        t.key = base_key .. "_jumbo_" .. index
+        t.pos = pos
+        t.config = { extra = pack_size + 2, choose = 1 }
+        t.cost = 6
+        SMODS.Booster(t)
+    end
+    for index, pos in ipairs(mega_poses) do
+        local t = copy_table(common_values)
+        t.key = base_key .. "_mega_" .. index
+        t.pos = pos
+        t.config = { extra = pack_size + 2, choose = 2 }
+        t.cost = 8
+        SMODS.Booster(t)
+    end
+end
