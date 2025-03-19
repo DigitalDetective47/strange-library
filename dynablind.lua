aa9ef8919da23ab686fc6b4af6af4177b1dcec45
DigitalDetective47 <ninji2701@gmail.com>
2025-03-19 04:45:10 -0400

    add annotations

--- dynablind.lua
+++ dynablind.lua
@@ -1,3 +1,5 @@
+StrangeLib.dynablind = {}
+
 G.FUNCS.StrangeLib_blind_UI_scale = function(e)
     e.config.scale = scale_number(StrangeLib.dynablind.blind_choice_scores[e.config.ref_value], 0.7, 100000)
 end
@@ -33,50 +35,64 @@ function Game:start_run(args)
     StrangeLib.dynablind.update_blind_scores()
 end
 
-return {
-    blind_choice_scores = {},
-    blind_choice_score_texts = {},
-    find_blind = function(key, count_defeated, count_skipped)
-        local found = {}
-        for _, blind_choice in ipairs({ "Small", "Big", "Boss" }) do
-            if G.GAME.round_resets.blind_choices[blind_choice] == key
-                and (count_defeated or G.GAME.round_resets.blind_states[blind_choice] ~= "Defeated")
-                and (count_skipped or G.GAME.round_resets.blind_states[blind_choice] ~= "Skipped") then
-                found[blind_choice] = true
-            end
-        end
-        return found
-    end,
-    get_blind_score = function(blind, base)
-        base = base or get_blind_amount(G.GAME.round_resets.blind_ante) * G.GAME.starting_params.ante_scaling
-        if blind.score then
-            return blind:score(base)
-        else
-            return base * blind.mult
-        end
-    end,
-    update_blind_scores = function(which)
-        which = which or { Small = true, Big = true, Boss = true }
-        for blind_choice, update in pairs(which) do
-            if update then
-                StrangeLib.dynablind.blind_choice_scores[blind_choice] = StrangeLib.dynablind.get_blind_score(G.P_BLINDS
-                    [G.GAME.round_resets.blind_choices[blind_choice]])
-                StrangeLib.dynablind.blind_choice_score_texts[blind_choice] =
-                    number_format(StrangeLib.dynablind.blind_choice_scores[blind_choice])
-            end
-        end
-        if G.GAME.blind.chips ~= 0 and which[G.GAME.blind_on_deck] then
-            G.GAME.blind.chips = StrangeLib.dynablind.get_blind_score(G.GAME.blind)
-            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    if G.GAME.chips >= G.GAME.blind.chips then
-                        G.STATE = G.STATES.NEW_ROUND
-                        G.STATE_COMPLETE = false
-                    end
-                    return true
-                end
-            }))
+---@type { Small: number, Big: number, Boss: number }
+StrangeLib.dynablind.blind_choice_scores = {}
+---@type { Small: string, Big: string, Boss: string }
+StrangeLib.dynablind.blind_choice_score_texts = {}
+
+---Get what slots are occupied by a given blind
+---@param key string
+---@param count_defeated boolean?
+---@param count_skipped boolean?
+---@return { Small: boolean?, Big: boolean?, Boss: boolean? }
+function StrangeLib.dynablind.find_blind(key, count_defeated, count_skipped)
+    local found = {}
+    for _, blind_choice in ipairs({ "Small", "Big", "Boss" }) do
+        if G.GAME.round_resets.blind_choices[blind_choice] == key
+            and (count_defeated or G.GAME.round_resets.blind_states[blind_choice] ~= "Defeated")
+            and (count_skipped or G.GAME.round_resets.blind_states[blind_choice] ~= "Skipped") then
+            found[blind_choice] = true
         end
     end
-}
+    return found
+end
+
+---Get the required score for a given Boss Blind
+---@param blind Blind
+---@param base number? defaults to the current base score
+---@return number
+function StrangeLib.dynablind.get_blind_score(blind, base)
+    base = base or get_blind_amount(G.GAME.round_resets.blind_ante) * G.GAME.starting_params.ante_scaling
+    if blind.score then
+        return blind:score(base)
+    else
+        return base * blind.mult
+    end
+end
+
+---Update displayed scores
+---@param which { Small: boolean?, Big: boolean?, Boss: boolean? }? blinds to update; defaults to all blinds
+function StrangeLib.dynablind.update_blind_scores(which)
+    which = which or { Small = true, Big = true, Boss = true }
+    for blind_choice, update in pairs(which) do
+        if update then
+            StrangeLib.dynablind.blind_choice_scores[blind_choice] = StrangeLib.dynablind.get_blind_score(G.P_BLINDS
+                [G.GAME.round_resets.blind_choices[blind_choice]])
+            StrangeLib.dynablind.blind_choice_score_texts[blind_choice] =
+                number_format(StrangeLib.dynablind.blind_choice_scores[blind_choice])
+        end
+    end
+    if G.GAME.blind.chips ~= 0 and which[G.GAME.blind_on_deck] then
+        G.GAME.blind.chips = StrangeLib.dynablind.get_blind_score(G.GAME.blind)
+        G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                if G.GAME.chips >= G.GAME.blind.chips then
+                    G.STATE = G.STATES.NEW_ROUND
+                    G.STATE_COMPLETE = false
+                end
+                return true
+            end
+        }))
+    end
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
+StrangeLib = {}
 SMODS.Atlas({
     key = "modicon",
     path = "icon.png",
     px = 34,
     py = 34,
 })
-StrangeLib = { dynablind = SMODS.load_file("dynablind.lua")() }
 
+SMODS.load_file("dynablind.lua")()
+
+---@param x number | table
+---@param op "==" | "~=" | "<" | "<=" | ">" | ">="
+---@param y number | table
+---@return boolean
 local function compare(x, op, y)
     if op == "==" then
         return x == y
@@ -23,6 +29,13 @@ local function compare(x, op, y)
         sendErrorMessage('invalid operator "' .. op .. '"', "StrangeLib.safe_compare")
     end
 end
+
+---Compare two numbers safely when Talisman may or may not be present.
+---@param x number | table
+---@param op "==" | "~=" | "<" | "<=" | ">" | ">="
+---@param y number | table
+---@return boolean
+function StrangeLib.safe_compare(x, op, y) end
 
 if next(SMODS.find_mod("Talisman")) then
     function StrangeLib.safe_compare(x, op, y)
@@ -43,6 +56,10 @@ else
     end
 end
 
+---Append all elements of src to dest
+---@generic T
+---@param dest T[] the array to add to
+---@param src T[] list of items to append
 function StrangeLib.bulk_add(dest, src)
     local old_length = #dest
     for index, item in ipairs(src) do
@@ -52,6 +69,13 @@ end
 
 SMODS.load_file("fcalc.lua")()
 
+---@alias Pos { x: integer, y: integer }
+---@param base_key string used to generate the actual booster pack keys
+---@param normal_poses Pos[] atlas positions for normal booster packs
+---@param jumbo_poses Pos[] atlas positions for jumbo booster packs
+---@param mega_poses Pos[] atlas positions for mega booster packs
+---@param common_values table entries to be included in all booster packs
+---@param pack_size integer? the base size of packs; defaults to 3
 function StrangeLib.make_boosters(base_key, normal_poses, jumbo_poses, mega_poses, common_values, pack_size)
     pack_size = pack_size or 3
     for index, pos in ipairs(normal_poses) do
