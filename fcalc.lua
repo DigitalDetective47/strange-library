183a4d8bc7824edc56d4cbfe80982dee03e0d5c4
DigitalDetective47 <ninji2701@gmail.com>
2025-11-07 06:46:13 -0500

    fix fcalc

--- fcalc.lua
+++ fcalc.lua
@@ -1,23 +1,32 @@
--- individual inserts required for message to be timed correctly
-table.insert(SMODS.calculation_keys, 1,"f_chips")
-table.insert(SMODS.calculation_keys, 1,"fchips")
-table.insert(SMODS.calculation_keys, 1,"f_mult")
-table.insert(SMODS.calculation_keys, 1,"fmult")
-table.insert(SMODS.calculation_keys, 1,"f_chips_mult")
+StrangeLib.bulk_add(SMODS.Scoring_Parameters.chips.calculation_keys, { "f_chips", "fchips" })
+local chip_calc_hook = SMODS.Scoring_Parameters.chips.calc_effect
+function SMODS.Scoring_Parameters.chips.calc_effect(self, effect, scored_card, key, amount, from_edition)
+    if (key == "f_chips" or key == "fchips") and type(amount) == "function" then
+        if effect.card and effect.card ~= scored_card then juice_card(effect.card) end
+        self:modify(amount(hand_chips) - hand_chips)
+        return true
+    end
+    return chip_calc_hook(self, effect, scored_card, key, amount, from_edition)
+end
+
+StrangeLib.bulk_add(SMODS.Scoring_Parameters.chips.calculation_keys, { "f_mult", "fmult" })
+local mult_calc_hook = SMODS.Scoring_Parameters.mult.calc_effect
+function SMODS.Scoring_Parameters.mult.calc_effect(self, effect, scored_card, key, amount, from_edition)
+    if (key == "f_mult" or key == "fmult") and type(amount) == "function" then
+        if effect.card and effect.card ~= scored_card then juice_card(effect.card) end
+        self:modify(amount(mult) - mult)
+        return true
+    end
+    return mult_calc_hook(self, effect, scored_card, key, amount, from_edition)
+end
 
 local calculate_individual_effect_hook = SMODS.calculate_individual_effect
 function SMODS.calculate_individual_effect(effect, scored_card, key, amount, from_edition)
-    if key == "f_chips" or key == "fchips" then
-        hand_chips = amount(hand_chips)
-        update_hand_text({ delay = 0 }, { chips = hand_chips, mult = mult })
-        return true
-    elseif key == "f_mult" or key == "fmult" then
-        mult = amount(mult)
-        update_hand_text({ delay = 0 }, { chips = hand_chips, mult = mult })
-        return true
-    elseif key == "f_chips_mult" then
-        hand_chips, mult = amount(hand_chips, mult)
-        update_hand_text({ delay = 0 }, { chips = hand_chips, mult = mult })
+    if key == "f_chips_mult" then
+        if effect.card and effect.card ~= scored_card then juice_card(effect.card) end
+        local new_chips, new_mult = amount(hand_chips, mult)
+        SMODS.Scoring_Parameters.chips:modify(new_chips - hand_chips)
+        SMODS.Scoring_Parameters.mult:modify(new_mult - mult)
         return true
     end
     return calculate_individual_effect_hook(effect, scored_card, key, amount, from_edition)
